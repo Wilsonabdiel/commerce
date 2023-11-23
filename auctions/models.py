e743851e3e16cb4340c65c111147ee38e1160f48
Wilsonabdiel <wilsonabdiel86@gmail.com>
2023-11-23 13:26:58 +0100

    feat:models.py

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,26 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+from django.db import models
+# from django.contrib.auth.models import User
 
 class User(AbstractUser):
     pass
+
+class AuctionListing(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    starting_bid = models.DecimalField(max_digits=10, decimal_places=2)
+    reserve_price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
+    end_time = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image_url = models.URLField(blank=True)
+
+class Bid(models.Model):
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    auction_listing = models.ForeignKey(AuctionListing, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    content = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    auction_listing = models.ForeignKey(AuctionListing, on_delete=models.CASCADE)
